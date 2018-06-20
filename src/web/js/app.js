550a53b7ec9581769c6eec26951c87cca41cde09
lisar01 <lisar.3467@gmail.com>
2018-06-20 14:25:15 -0300

    borrado el viejo modelo de la parte web del proyecto

--- src/web/js/app.js
+++
@@ -1,7 +0,0 @@
-
-angular.module("LinkSportsApp", ["ui-router"])
-    .service('UserService', UserService)
-    .controller('UserController', UserController)
-    .controller('SearchController', SearchController)
-
-.config(routes);
