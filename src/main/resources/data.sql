a42ebb0e7baae8ec31770484c3e7adcf4a604e46
lisar01 <lisar.3467@gmail.com>
2018-06-22 19:49:50 -0300

    modificado los statements de los inserts del data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO user
 (id, username, password, nombre, apellido, deporte, tipo)
 VALUES
-  (10,'undeportista1', '123456', 'nombredeportista1', 'apellidodeportista1', 'Football', 'Atleta'),
-  (11,'undeportista2', '123456', 'nombredeportista2', 'apellidodeportista2', 'Tenis', 'Atleta'),
-  (3, 'undeportista3', 'qwerty', 'nombredeportista3', 'apellidodeportista3', 'Football', 'Atleta'),
-  (4, 'undeportista4', 'qwerty', 'nombredeportista4', 'apellidodeportista4', 'Tenis', 'Atleta'),
-  (5, 'undeportista5', '7890', 'nombredeportista5', 'apellidodeportista5', 'Basketball', 'Atleta'),
-  (6, 'undeportista6', '7890', 'nombredeportista6', 'apellidodeportista6', 'Handball', 'Atleta'),
-  (7, 'undeportista7', 'asdfg', 'nombredeportista7', 'apellidodeportista7', 'Tenis', 'Atleta'),
-  (8, 'admin', 'admin', 'nombredeportista9', 'apellidodeportista9', 'Golf', 'Atleta'),
-  (9, 'undeportista8', 'asdfg', 'nombredeportista8', 'apellidodeportista8', 'Voleyball', 'Atleta');
+  (210,'undeportista1', '123456', 'Federico', 'Sanchez', 'Football', 'Atleta'),
+  (111,'undeportista2', '123456', 'Pablo', 'Mayer', 'Tenis', 'Atleta'),
+  (23, 'undeportista3', 'qwerty', 'Tito', 'Martinez', 'Football', 'Atleta'),
+  (34, 'undeportista4', 'qwerty', 'Juan Martin', 'Del Potro', 'Tenis', 'Atleta'),
+  (55, 'undeportista5', '7890', 'Maria', 'Gomez', 'Basketball', 'Atleta'),
+  (66, 'undeportista6', '7890', 'Leonardo', 'Gutierrez', 'Handball', 'Coach'),
+  (77, 'undeportista7', 'asdfg', 'John', 'Ruiz', 'Tenis', 'Coach'),
+  (18, 'admin', 'admin', 'Gabriel', 'Soliz', 'Golf', 'Coach'),
+  (39, 'undeportista8', 'asdfg', 'Ana Sol', 'Peña', 'Voleyball', 'Coach');
