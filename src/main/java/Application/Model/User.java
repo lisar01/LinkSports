dc78e76688ae37ad1113d8f80047393222cd525c
lisar01 <lisar.3467@gmail.com>
2018-06-28 11:23:11 -0300

    fix a update del server, test de update en cucumber y el usuario ya no tiene id

--- src/main/java/Application/Model/User.java
+++ src/main/java/Application/Model/User.java
@@ -6,10 +6,8 @@ import java.util.List;
 
 @Entity
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    @Column(nullable = false)
     private String username;
     private String password;
     private String nombre;
@@ -30,27 +28,51 @@ public class User {
         this.tipo = tipo;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
 
     public void setDeporte(String deporte) {
         this.deporte = deporte;
     }
-    public long getId() {
-        return this.id;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getNombre() {
-        return nombre;
-    }
-    public String getApellido() { return apellido; }
-    public String getDeporte() {
-        return deporte;
-    }
-    public String getTipo() { return tipo; }
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
